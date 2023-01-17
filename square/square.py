06bf65a41734b375f0b46468d0ed2b4cff209792
chloe-healy <81374640+chloe-healy@users.noreply.github.com>
2023-01-17 12:47:35 +0000

    Add files via upload

---
+++ square/square.py
@@ -0,0 +1,7 @@
+def square(x: int, y: int, z: int, a:int) -> str:
+    if x == y == z == a:
+        return "Square"
+    elif x == y and z == a:
+        return "Rectangle"
+    else:
+        return "Not a Square"
