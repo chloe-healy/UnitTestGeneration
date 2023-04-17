a53ae49389f60153fec39be2435ce5a2f6a70597
chloe-healy <81374640+chloe-healy@users.noreply.github.com>
2023-04-17 15:04:55 +0100

    Delete circle.py

--- circle.py
+++
@@ -1,6 +0,0 @@
-def circle(radius: float) -> float:
-    pi = 3.14159
-    area = pi * radius ** 2
-    circumference = 2 * pi * radius
-    return f"Area: {area:.2f}, Circumference: {circumference:.2f}"
-    
