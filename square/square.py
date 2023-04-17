caf9063f9d53ca2b2e0eb52ffade3d0ff05fe65e
chloe-healy <81374640+chloe-healy@users.noreply.github.com>
2023-04-17 16:28:45 +0100

    Update square.py

--- square/square.py
+++ square/square.py
@@ -1,5 +1,5 @@
 def square(x: int, y: int, z: int, a:int) -> str:
-    if x == y == z == a:
+    if x == y == z == a
         return "Square"
     elif x == y and z == a:
         return "Rectangle"
